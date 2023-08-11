7f9f267a775250bb443469a04d19382f740263e5
Bani Somo <banisomo2003@gmail.com>
2023-08-11 15:17:58 -0400

    Modify models for data persistence

--- src/main/java/sia/tacocloud/Taco.java
+++ src/main/java/sia/tacocloud/Taco.java
@@ -1,16 +1,23 @@
 package sia.tacocloud;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+    private Long id;
+    private Date createdAt = new Date();
+
     @NotNull
-    @Size(min = 3, message = "Name must be at least 3 characters long")
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 3, max = 25, message = "Name must be 3-25 characters long")
     private String name;
+
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
--- src/main/java/sia/tacocloud/TacoOrder.java
+++ src/main/java/sia/tacocloud/TacoOrder.java
@@ -7,25 +7,39 @@ import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
 import lombok.Data;
+
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data // Provides toString, hashCode and equals methods to the class
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+    private static final long serializedVersionUID = 1L;
+    private Long id;
+    private Date placedAt;
+
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
+
     @NotBlank(message = "Street is required")
     private String deliveryStreet;
+
     @NotBlank(message = "City is required")
     private String deliveryCity;
+
     @NotBlank(message = "State is required")
     private String deliveryState;
+
     @NotBlank(message = "Zip code is required")
     private String deliveryZip;
+
     @CreditCardNumber(message = "Invalid credit card number")
     private String ccNumber;
+
     @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
+
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
