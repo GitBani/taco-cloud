74024e1932e9eec80422181152af27ae9d692f40
Bani Somo <banisomo2003@gmail.com>
2023-09-07 20:15:15 -0400

    Add more data in the command line runner

--- src/main/java/sia/tacocloud/TacoCloudApplication.java
+++ src/main/java/sia/tacocloud/TacoCloudApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import sia.tacocloud.data.IngredientRepository;
 import sia.tacocloud.Ingredient.Type;
+import sia.tacocloud.data.TacoRepository;
 import sia.tacocloud.data.UserRepository;
+
+import java.util.Arrays;
 
 // This is essentially boilerplate to run the application
 @SpringBootApplication
@@ -17,18 +21,50 @@ public class TacoCloudApplication {
 	}
 
 	@Bean
-	public CommandLineRunner dataLoader(IngredientRepository ingredientRepository) {
+	public CommandLineRunner dataLoader(
+			IngredientRepository ingredientRepository,
+			TacoRepository tacoRepository,
+			UserRepository userRepository,
+			PasswordEncoder passwordEncoder
+	) {
 		return args -> {
-			ingredientRepository.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-			ingredientRepository.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-			ingredientRepository.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-			ingredientRepository.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-			ingredientRepository.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-			ingredientRepository.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-			ingredientRepository.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
-			ingredientRepository.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-			ingredientRepository.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
-			ingredientRepository.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+			Ingredient flourTortilla = new Ingredient("FLTO", "Flour Tortilla", Type.WRAP);
+			Ingredient cornTortilla = new Ingredient("COTO", "Corn Tortilla", Type.WRAP);
+			Ingredient groundBeef = new Ingredient("GRBF", "Ground Beef", Type.PROTEIN);
+			Ingredient carnitas = new Ingredient("CARN", "Carnitas", Type.PROTEIN);
+			Ingredient dicedTomatoes = new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES);
+			Ingredient lettuce = new Ingredient("LETC", "Lettuce", Type.VEGGIES);
+			Ingredient cheddar = new Ingredient("CHED", "Cheddar", Type.CHEESE);
+			Ingredient monterreyJack = new Ingredient("JACK", "Monterrey Jack", Type.CHEESE);
+			Ingredient salsa = new Ingredient("SLSA", "Salsa", Type.SAUCE);
+			Ingredient sourCream = new Ingredient("SRCR", "Sour Cream", Type.SAUCE);
+
+			ingredientRepository.save(flourTortilla);
+			ingredientRepository.save(cornTortilla);
+			ingredientRepository.save(groundBeef);
+			ingredientRepository.save(carnitas);
+			ingredientRepository.save(dicedTomatoes);
+			ingredientRepository.save(lettuce);
+			ingredientRepository.save(cheddar);
+			ingredientRepository.save(monterreyJack);
+			ingredientRepository.save(salsa);
+			ingredientRepository.save(sourCream);
+
+			Taco taco1 = new Taco();
+			taco1.setName("Sheer Heart Attack");
+			taco1.setIngredients(Arrays.asList(flourTortilla, groundBeef, carnitas, cheddar, monterreyJack, sourCream));
+			tacoRepository.save(taco1);
+
+
+			Taco taco2 = new Taco();
+			taco2.setName("c h e e s e");
+			taco2.setIngredients(Arrays.asList(cornTortilla, cheddar, monterreyJack));
+			tacoRepository.save(taco2);
+
+			Taco taco3 = new Taco();
+			taco3.setName("Vegan");
+			taco3.setIngredients(Arrays.asList(cornTortilla, dicedTomatoes, lettuce, salsa, sourCream));
+			tacoRepository.save(taco3);
 		};
 	}
 }
